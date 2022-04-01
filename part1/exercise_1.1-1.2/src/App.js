b5d4d27a7de5ed0a4dd1e093b1fd687105d959e7
sinanju <pedroubine@hotmail.com>
2022-04-01 09:20:37 -0300

    Coisa besta

--- part1/exercise_1.1-1.2/src/App.js
+++ part1/exercise_1.1-1.2/src/App.js
@@ -1,8 +1,21 @@
-const App = (props) => {
-    const {counter} = props
+import { useState } from "react"
+
+
+const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => { setAll(allClicks.concat('L')); setLeft(left + 1)  }
+    const handleRightClick = () => { setAll(allClicks.concat('R')); setRight(right + 1)  }
     return (
-      <div>{counter}</div>
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <p>{allClicks.join(' ')}</p>    </div>
     )
   }
-  
-  export default App
+
+export default App
--- part1/exercise_1.1-1.2/src/index.js
+++ part1/exercise_1.1-1.2/src/index.js
@@ -1,15 +1,8 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 
-let counter = 1
+ReactDOM.render(<App />, 
+document.getElementById('root'))
 
-const refresh = () => {
-  ReactDOM.render(<App counter={counter} />, 
-  document.getElementById('root'))
-}
 
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
+
